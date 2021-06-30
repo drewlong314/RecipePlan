ce296024d2fb254338164cf666e4ff9f27b9f077
Drew Long <drewlong314@gmail.com>
2021-06-30 14:59:59 -0500

    create recipes front end

--- react-app/src/components/RecipeCreate/index.js
+++ react-app/src/components/RecipeCreate/index.js
@@ -1,9 +1,96 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { postRecipe } from "../../store/recipes";
 
 const RecipeCreate = () => {
-  return <div>
-      <h1>Recipe Creation Page </h1>
-  </div>
-}
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.session.user);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [image, setImage] = useState("");
+  const [servings, setServings] = useState(0);
+  const [time, setTime] = useState(0);
+  const [instructions, setInstructions] = useState("");
 
-export default RecipeCreate
+  const createRecipe = (e) => {
+    e.preventDefault();
+    dispatch(
+      postRecipe(
+        name,
+        description,
+        image,
+        servings,
+        time,
+        instructions,
+        user.id
+      )
+    );
+  };
+
+  return (
+    <div>
+      <h1>Recipe Creation Page </h1>
+      <form onSubmit={createRecipe}>
+        <div>
+          <label>Recipe Name</label>
+          <input
+            type="text"
+            name="name"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          ></input>
+        </div>
+        <div>
+          <label>Recipe Description</label>
+          <textarea
+            type="text"
+            name="description"
+            onChange={(e) => setDescription(e.target.value)}
+            value={description}
+          ></textarea>
+        </div>
+        <div>
+          <label>Recipe Image</label>
+          <input
+            type="text"
+            name="image"
+            onChange={(e) => setImage(e.target.value)}
+            value={image}
+          ></input>
+        </div>
+        <div>
+          <label>Recipe Servings</label>
+          <input
+            type="number"
+            name="servings"
+            onChange={(e) => setServings(e.target.value)}
+            value={servings}
+          ></input>
+        </div>
+        <div>
+          <label>Time Needed To Cook</label>
+          <input
+            type="number"
+            name="time"
+            onChange={(e) => setTime(e.target.value)}
+            value={time}
+          ></input>
+        </div>
+        <div>
+          <label>Recipe Instructions</label>
+          <textarea
+            type="text"
+            name="instructions"
+            onChange={(e) => setInstructions(e.target.value)}
+            value={instructions}
+          ></textarea>
+        </div>
+        <div>
+          <button type="submit">Create Recipe</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default RecipeCreate;
