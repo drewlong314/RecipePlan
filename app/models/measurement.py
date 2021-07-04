f8badb78d908dd8c21e2a1b4de97556a45a155c9
Drew Long <drewlong314@gmail.com>
2021-07-03 20:33:44 -0500

    measurements seeder

--- app/models/measurement.py
+++ app/models/measurement.py
@@ -6,6 +6,6 @@ class Measurement(db.Model):
     __tablename__ = 'measurements'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
+    name = db.Column(db.String)
 
     recipe_ingredients = db.relationship("Recipe_Ingredient")
