c0fdd785b3c6a64d2dd16e3637eb1004e9ba6e68
Drew Long <drewlong314@gmail.com>
2021-07-06 17:18:36 -0500

    delete recipe_ingredient from db

--- app/api/ingredient_routes.py
+++ app/api/ingredient_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request
-from app.models import Ingredient, Recipe_Ingredient
+from app.models import db, Ingredient, Recipe_Ingredient
 
 ingredient_routes = Blueprint('ingredients', __name__)
 
@@ -15,5 +15,7 @@ def delete_recipe_ingredient():
     print(request.get_json(), 'eieieieieieieieieieieieieiieieieieieieieieie')
     r = request.get_json()
     recipe_ingredient_to_delete = Recipe_Ingredient.query.filter_by(ingredient_id=r['ingredient_id'], recipe_id=r['recipe_id']).first()
+    db.session.delete(recipe_ingredient_to_delete)
+    db.session.commit()
     print('This will be deleted', recipe_ingredient_to_delete)
     return request.get_json()
