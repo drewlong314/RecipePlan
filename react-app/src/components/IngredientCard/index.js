e0a77285422d37c707e281a945af15a9530b9439
Drew Long <drewlong314@gmail.com>
2021-07-07 11:15:26 -0500

    edit ingredient frontend

--- react-app/src/components/IngredientCard/index.js
+++ react-app/src/components/IngredientCard/index.js
@@ -1,29 +1,89 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { removeCurrentIngredient, removeIngredient } from "../../store/ingredients";
+import {
+  removeCurrentIngredient,
+  removeIngredient,
+} from "../../store/ingredients";
 
-const IngredientCard = ({ quantity, measurement, ingredient, identifier, ingredient_id, recipe_id }) => {
+const IngredientCard = ({
+  quantity,
+  measurement,
+  ingredient,
+  identifier,
+  ingredient_id,
+  recipe_id,
+}) => {
   const dispatch = useDispatch();
   const currentIngredients = useSelector(
     (state) => state.ingredientReducer.current
   );
+  const measurements = useSelector(
+    (state) => state.measurementReducer.measurements
+  );
+  const [quantityUsed, setQuantityUsed] = useState(quantity)
+  const [measurementUsed, setMeasurementUsed] = useState(measurement)
+  const [ingredientUsed, setIngredientUsed] = useState(ingredient)
+  const [show, setShow] = useState(false);
+
+  // useEffect(() => {
+
+  // }, [quantityUsed, measurementUsed, ingredientUsed])
+
   const removeAnIngredient = () => {
     const ingredientToRemove = currentIngredients.filter((ingredient) => {
       return ingredient.props.identifier === identifier;
     });
     if (recipe_id) dispatch(removeIngredient(ingredientToRemove[0]));
-    else dispatch(removeCurrentIngredient(ingredientToRemove[0]))
+    else dispatch(removeCurrentIngredient(ingredientToRemove[0]));
   };
+  const editIngredient = () => {
+    setShow(true);
+  };
+  const submitEditIngredient = () => {
+    setShow(false)
+  }
   return (
     <div>
-      <li>
-        {`${quantity} ${measurement} ${ingredient}`}
-        <span>
-          <button onClick={removeAnIngredient} type="button">
-            Remove Ingredient
-          </button>
-        </span>
-      </li>
+      {show === false && (
+        <li>
+          {`${quantityUsed} ${measurementUsed} ${ingredientUsed}`}
+          <span>
+            <button onClick={editIngredient} type="button">
+              Edit
+            </button>
+            <button onClick={removeAnIngredient} type="button">
+              Remove
+            </button>
+          </span>
+        </li>
+      )}
+      {show === true && (
+        <div>
+          <input
+              type="number"
+              value={quantityUsed}
+              onChange={(e) => setQuantityUsed(e.target.value)}
+            ></input>
+            <select
+              value={measurementUsed}
+              onChange={(e) => {
+                setMeasurementUsed(e.target.value);
+              }}
+            >
+              {measurements.map((measurement) => {
+                return (
+                  <option key={measurement.name} value={measurement.name}>{measurement.name}</option>
+                );
+              })}
+            </select>
+            <input
+              onChange={(e) => setIngredientUsed(e.target.value)}
+              placeholder="eggs"
+              value={ingredientUsed}
+            ></input>
+            <button onClick={submitEditIngredient}>Submit</button>
+        </div>
+      )}
     </div>
   );
 };
