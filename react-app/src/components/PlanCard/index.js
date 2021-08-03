db87904bc8e4590d45e555357d8852a1396e0786
Drew Long <drewlong314@gmail.com>
2021-08-03 17:16:35 -0500

    basic meal plan links

--- react-app/src/components/PlanCard/index.js
+++ react-app/src/components/PlanCard/index.js
@@ -1,41 +1,49 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
+import "./style.css";
 
 const PlanCard = ({ day, dayName }) => {
   const recipes = useSelector((state) => state.recipeReducer.recipes);
-  let breakfast = "";
-  let lunch = "";
-  let dinner = "";
-  let dessert = "";
+  let breakfast = <h1 className={"calendar-not"}>None</h1>;
+  let lunch = <h1 className={"calendar-not"}>None</h1>;
+  let dinner = <h1 className={"calendar-not"}>None</h1>;
+  let dessert = <h1 className={"calendar-not"}>None</h1>;
   if (day.length) {
     day.forEach((recipe) => {
-      console.log(recipe.plan_category);
-      if (recipe.plan_category === "Breakfast") breakfast = recipe.name;
-      else if (recipe.plan_category === "Lunch") lunch = recipe.name;
-      else if (recipe.plan_category === "Dinner") dinner = recipe.name;
-      else if (recipe.dessert === "Dessert") dessert = recipe.name;
+      if (recipe.plan_category === "Breakfast")
+        breakfast = (
+          <a href={recipe.id}>
+            <img className={"calendar-image"} src={recipe.image} />
+          </a>
+        );
+      else if (recipe.plan_category === "Lunch")
+        lunch = (
+          <a href={recipe.id}>
+            <img className={"calendar-image"} src={recipe.image} />
+          </a>
+        );
+      else if (recipe.plan_category === "Dinner")
+        dinner = (
+          <a href={recipe.id}>
+            <img className={"calendar-image"} src={recipe.image} />
+          </a>
+        );
+      else if (recipe.dessert === "Dessert")
+        dessert = (
+          <a href={recipe.id}>
+            <img className={"calendar-image"} src={recipe.image} />
+          </a>
+        );
     });
   }
   return (
     <div>
       <div>
-        <h1>{dayName}</h1>
-        <div>
-          <h1>Breakfast</h1>
-          <h2>{breakfast}</h2>
-        </div>
-        <div>
-          <h1>Lunch</h1>
-          <h2>{lunch}</h2>
-        </div>
-        <div>
-          <h1>Dinner</h1>
-          <h2>{dinner}</h2>
-        </div>
-        <div>
-          <h1>Dessert</h1>
-          <h2>{dessert}</h2>
-        </div>
+        <h1 className={"calendar-name"}>{dayName}</h1>
+        <div className={"calendar-box"}>{breakfast}</div>
+        <div className={"calendar-box"}>{lunch}</div>
+        <div className={"calendar-box"}>{dinner}</div>
+        <div className={"calendar-box"}>{dessert}</div>
       </div>
     </div>
   );
