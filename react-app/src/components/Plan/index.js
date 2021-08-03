3735bc55de4648fcc3e7f3757eac360e47709ebf
Drew Long <drewlong314@gmail.com>
2021-08-03 12:01:40 -0500

    add CSS layout for plan

--- react-app/src/components/Plan/index.js
+++ react-app/src/components/Plan/index.js
@@ -1,17 +1,30 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import PlanCard from "../PlanCard";
+import "./style.css";
 
 const Plan = () => {
   const recipes = useSelector((state) => state.recipeReducer.recipes);
   console.log(recipes);
   let monday = [];
+  let tuesday = [];
+  let wednesday = [];
+  let thursday = [];
+  let friday = [];
+  let saturday = [];
+  let sunday = [];
   const daySorter = recipes?.forEach((recipe) => {
     if (recipe.day === "Monday") monday.push(recipe);
   });
   return (
-    <div>
-        <PlanCard day={monday}/>
+    <div className={"calendar"}>
+      <PlanCard day={monday} dayName={'Monday'} />
+      <PlanCard day={tuesday} dayName={'Tuesday'}/>
+      <PlanCard day={wednesday} dayName={'Wednesday'}/>
+      <PlanCard day={thursday} dayName={'Thursday'}/>
+      <PlanCard day={friday} dayName={'Friday'}/>
+      <PlanCard day={saturday} dayName={'Saturday'}/>
+      <PlanCard day={sunday} dayName={'Sunday'}/>
     </div>
   );
 };
