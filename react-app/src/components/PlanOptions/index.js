fafda43e963d1fa18ecadae81e0339434f1f16e8
Drew Long <drewlong314@gmail.com>
2021-08-03 20:31:42 -0500

    add modal

--- react-app/src/components/PlanOptions/index.js
+++ react-app/src/components/PlanOptions/index.js
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
+import CalendarModal from "../CalendarModal";
 
 const PlanOptions = ({ meal }) => {
   const [options, setOptions] = useState(false);
+  const [modal, setModal] = useState(false)
+  const addRecipe = () => {
+      setModal(true)
+      // this will open the modal that contains the search bar
+  }
   return (
     <div className={"calendar-box"}>
       {options ? (
         <div className={"calendar-options"}>
           <button className={"calendar-options__x"} onClick={(e) => setOptions(false)}>X</button>
-          <button className={"calendar-options__button"}>Add recipe</button>
+          <CalendarModal />
+          {/* <button onClick={addRecipe} className={"calendar-options__button"}>Add recipe</button> */}
           <button className={"calendar-options__button"}>Remove recipe</button>
         </div>
       ) : (
