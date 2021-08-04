cf08c47527397c5a597ba9efa59aa24218991a00
Drew Long <drewlong314@gmail.com>
2021-08-04 16:24:36 -0500

    basic modal css

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,13 +4,16 @@ import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import configureStore from "./store";
+import { ModalProvider } from "./context/Modal";
 
 const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ModalProvider>
+        <App />
+      </ModalProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
